2e79d6781f6231d376092f87a12200ab5f17ed1a
liqi <973129554@qq.com>
2023-08-25 10:06:27 +0800

    fix arrayMax

--- lab2/DebugExercise/DebugExercise2.java
+++ lab2/DebugExercise/DebugExercise2.java
@@ -40,6 +40,9 @@ public class DebugExercise2 {
      * and b = {0, 20, 5}, this function will return {1, 20, 5}.
      * */
     public static int[] arrayMax(int[] a, int[] b) {
+        if (a == null || b == null) {
+            return null;
+        }
         if (a.length != b.length) {
             System.out.println("ERROR! Arrays don't match");
             return null;
