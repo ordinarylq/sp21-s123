2d1be969da79b34cfb64d22d4d04d9c9b88efd24
liqi <973129554@qq.com>
2023-09-04 11:16:17 +0800

    add max array deque

---
+++ proj1/deque/MaxArrayDeque.java
@@ -0,0 +1,38 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
+
+    private Comparator<T> comparator;
+
+    public MaxArrayDeque(Comparator<T> comparator) {
+        super();
+        this.comparator = comparator;
+    }
+
+    public T max() {
+        int size = size();
+        T maxItem = get(0);
+        for (int i = 1; i < size; i++) {
+            T t = get(i);
+            if (comparator.compare(maxItem, t) < 0) {
+               maxItem = t;
+            }
+        }
+        return maxItem;
+    }
+
+    public T max(Comparator<T> c) {
+        int size = size();
+        T maxItem = get(0);
+        for (int i = 1; i < size; i++) {
+            T t = get(i);
+            if (c.compare(maxItem, t) < 0) {
+                maxItem = t;
+            }
+        }
+        return maxItem;
+    }
+
+}
